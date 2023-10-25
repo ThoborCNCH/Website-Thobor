f19d83ac8b5c5873e02904e3b4eafaa3641043cc
Alex <MaterniucAlex@gmail.com>
2023-10-25 16:13:53 +0300

    Fixed bug regarding apps page on mobile

--- src/components/apps/components/App.jsx
+++ src/components/apps/components/App.jsx
@@ -3,18 +3,18 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 
 function App( {image, name, adress}) {
   return (
-          <a href={adress} target="0">
-            <div className="card">
-            <LazyLoadImage className="card-front-image card-image" src={image} />
-              <div className="card-faders">
-                <LazyLoadImage className="card-fader card-image" src={image} />
-                <LazyLoadImage className="card-fader card-image" src={image} />
-                <LazyLoadImage className="card-fader card-image" src={image} />
-                <LazyLoadImage className="card-fader card-image" src={image} />
-              </div>
-            <h3>{name}</h3>
-            </div>
-          </a>
+    <div className="card">
+      <a href={adress} target="0">
+        <LazyLoadImage className="card-front-image card-image" src={image} />
+      </a>
+      <div className="card-faders">
+        <LazyLoadImage className="card-fader card-image" src={image} />
+        <LazyLoadImage className="card-fader card-image" src={image} />
+        <LazyLoadImage className="card-fader card-image" src={image} />
+        <LazyLoadImage className="card-fader card-image" src={image} />
+      </div>
+      <h3>{name}</h3>
+    </div>
   );
 }
 
