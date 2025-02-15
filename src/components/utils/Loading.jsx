e5d610ac7822149638cc21faf8fbd5bc2922589f
Crisstians <crisgh122@gmail.com>
2025-02-15 15:38:49 +0200

    Adaugat timeout la loading screen

--- src/components/utils/Loading.jsx
+++ src/components/utils/Loading.jsx
@@ -1,8 +1,18 @@
 import { useState, useEffect } from "react";
 
-const useLoadingManager = (totalImages) => {
+const useLoadingManager = (totalImages, timeoutDuration = 8000) => {
   const [loadedImages, setLoadedImages] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [timeoutReached, setTimeoutReached] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+      setTimeoutReached(true);
+    }, timeoutDuration);
+
+    return () => clearTimeout(timeout);
+  }, [timeoutDuration]);
 
   useEffect(() => {
     if (loadedImages >= totalImages) {
@@ -14,7 +24,7 @@ const useLoadingManager = (totalImages) => {
     setLoadedImages((prev) => prev + 1);
   };
 
-  return { isLoading, handleImageLoad };
+  return { isLoading, handleImageLoad, timeoutReached };
 };
 
 export default useLoadingManager;
